5e0ef2a566e69f59c28c1cfea60ce55c33d748e3
kn8a <mydreamhomefl@gmail.com>
2024-08-05 03:08:21 -0400

    added cart state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react"
 import { ChakraProvider, Box } from "@chakra-ui/react"
 import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
@@ -12,6 +12,17 @@ import ContactUs from "./pages/ContactUs"
 import "./App.css"
 
 const App = () => {
+
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) => {
+    setCart(prevCart => [...prevCart, item])
+  }
+
+  const removeFromCart = (itemId) => {
+    setCart(prevCart => prevCart.filter(item => item.id !== itemId))
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Router>
@@ -23,12 +34,12 @@ const App = () => {
           w={"100vw"}
           alignSelf={"center"}
         >
-          <Header />
+          <Header cartItemCount={cart.length}/>
           <Box flex={1}>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/products' element={<Products />} />
+              <Route path='/products' element={<Products addToCart={addToCart}/>} />
               <Route path='/global-reach' element={<GlobalReach />} />
               <Route path='/contact' element={<ContactUs />} />
             </Routes>
