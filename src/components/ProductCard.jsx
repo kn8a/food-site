643c6a6a81881b1c7ab28364a3585c63213da077
kn8a <mydreamhomefl@gmail.com>
2024-08-08 05:09:47 -0400

    remove unused imports disable prop validation

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 import {
   Box,
   Heading,
@@ -8,35 +8,35 @@ import {
   Wrap,
   Tag,
   Button,
-} from "@chakra-ui/react";
+} from "@chakra-ui/react"
 
 const ProductCard = ({ product, onLearnMore }) => (
-    <Box borderWidth={1} borderRadius='lg' overflow='hidden' p={4}>
-      <VStack spacing={4} align='start'>
-        <Image
-          src={`/food-site/products/${product.image}.jpg`}
-          alt={product.name}
-          borderRadius='md'
-          w='full'
-          cursor={"pointer"}
-          onClick={() => onLearnMore(product)}
-        />
-        <Heading as='h3' size='md'>
-          {product.name}
-        </Heading>
-        <Text>{product.description}</Text>
-        <Wrap>
-          {product.categories.map((category, index) => (
-            <Tag key={index} colorScheme='green'>
-              {category}
-            </Tag>
-          ))}
-        </Wrap>
-        <Button colorScheme='green' onClick={() => onLearnMore(product)}>
-          View Details
-        </Button>
-      </VStack>
-    </Box>
-  )
+  <Box borderWidth={1} borderRadius='lg' overflow='hidden' p={4}>
+    <VStack spacing={4} align='start'>
+      <Image
+        src={`/food-site/products/${product.image}.jpg`}
+        alt={product.name}
+        borderRadius='md'
+        w='full'
+        cursor={"pointer"}
+        onClick={() => onLearnMore(product)}
+      />
+      <Heading as='h3' size='md'>
+        {product.name}
+      </Heading>
+      <Text>{product.description}</Text>
+      <Wrap>
+        {product.categories.map((category, index) => (
+          <Tag key={index} colorScheme='green'>
+            {category}
+          </Tag>
+        ))}
+      </Wrap>
+      <Button colorScheme='green' onClick={() => onLearnMore(product)}>
+        View Details
+      </Button>
+    </VStack>
+  </Box>
+)
 
-  export default ProductCard;
+export default ProductCard
